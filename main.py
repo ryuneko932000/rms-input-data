fff7dfaa419466ef3814170a70e79afd2588f5e9
ryuneko932000 <kokeisnocturno2000@gmail.com>
2022-04-08 15:45:40 +0200

    Script is now able to read first line and save data in variables. Changed format encoding from ASCII to UTF-8. Added more data to des2.dat for testing purposes.

--- main.py
+++ main.py
@@ -2,15 +2,36 @@ import numpy as np
 import os
 
 
-print("Please type file to read: ")
+print("Escriba el archivo para leer: ")
 ftr=input("> ")
 
-print(f"File selected: {ftr}")
-print("To continue please press ENTER or CTRL-C to exit.")
+print(f"Archivo seleccionado: {ftr}")
+print("Para continuar presione ENTER o CTRL-C para salir.")
 input('> ')
 
-initialdata = np.genfromtxt(ftr, encoding='ASCII', dtype=None, names=('Nombre', 'p.m', 
-'Tc [K]', 'Pc [bar]', 'omega', 'kappa1','alfa0', 'alfa1', 'beta1', 'beta2', 'Ctv [cm3/gr]'),
-skip_header=1, max_rows=3)
+data = np.genfromtxt(ftr, encoding='UTF-8',dtype=None, names=('nombre', 'pm', 
+'tc', 'pc', 'omega', 'kappa1','alfa0', 'alfa1', 'beta1', 'beta2', 'ctv'),
+skip_header=1, max_rows=1)
+
+
+compuesto = data['nombre']
+pm = data['pm']
+tc = data['tc']
+pc = data['pc']
+omega = data['omega']
+kappa1 = data['kappa1']
+alfa0 = data['alfa0']
+alfa1 = data['alfa1']
+beta1 = data['beta1']
+beta2 = data['beta2']
+ctv = data['ctv']
+
+print(compuesto, pm, tc, pc, omega, kappa1, alfa0, alfa1, beta1, beta2, ctv)
+
+
+
+
+
+
 
 
