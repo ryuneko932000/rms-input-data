85603631e278b5636cbfe8788ba2ace76679d9f9
ryuneko932000 <kokeisnocturno2000@gmail.com>
2022-04-08 16:27:03 +0200

    Added feature to choose temp, as well as reading second set of data.

--- main.py
+++ main.py
@@ -9,24 +9,42 @@ print(f"Archivo seleccionado: {ftr}")
 print("Para continuar presione ENTER o CTRL-C para salir.")
 input('> ')
 
-data = np.genfromtxt(ftr, encoding='UTF-8',dtype=None, names=('nombre', 'pm', 
+initial_data = np.genfromtxt(ftr, encoding='UTF-8',dtype=None, names=('nombre', 'pm', 
 'tc', 'pc', 'omega', 'kappa1','alfa0', 'alfa1', 'beta1', 'beta2', 'ctv'),
 skip_header=1, max_rows=1)
 
+data2 = np.genfromtxt(ftr, encoding='UTF-8', dtype=None, names=('opcion', 'ede', 'funcionAlfa'), 
+skip_header=5, max_rows=7)
 
-compuesto = data['nombre']
-pm = data['pm']
-tc = data['tc']
-pc = data['pc']
-omega = data['omega']
-kappa1 = data['kappa1']
-alfa0 = data['alfa0']
-alfa1 = data['alfa1']
-beta1 = data['beta1']
-beta2 = data['beta2']
-ctv = data['ctv']
+
+compuesto = initial_data['nombre']
+pm = initial_data['pm']
+tc = initial_data['tc']
+pc = initial_data['pc']
+omega = initial_data['omega']
+kappa1 = initial_data['kappa1']
+alfa0 = initial_data['alfa0']
+alfa1 = initial_data['alfa1']
+beta1 = initial_data['beta1']
+beta2 = initial_data['beta2']
+ctv = initial_data['ctv']
+
+mtflag_option1 = data2[0]
+mtflag_option2 = data2[1]
+mtflag_option3 = data2[2]
+mtflag_option4 = data2[3]
+mtflag_option5 = data2[4]
+mtflag_option6 = data2[5]
+mtflag_option7 = data2[6]
+
+print(mtflag_option5['ede'])
+
+
+
 
 print(compuesto, pm, tc, pc, omega, kappa1, alfa0, alfa1, beta1, beta2, ctv)
+
+
 
 
 
